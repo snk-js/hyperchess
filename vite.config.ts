c1516452347401092eb72e4f0d1dd7aa3e38e64d
juliano <julianoh1909@gmail.com>
2023-08-08 14:01:47 -0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
-
+import { vitePreprocess } from '@sveltejs/kit/vite';
 const dev = process.argv.includes('dev');
 
+/** @type { import('@sveltejs/kit/vite')} */
 export default {
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
 		paths: {
