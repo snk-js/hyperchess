85a1274959f84d79175ad84ae3eb507ef2d9eae2
juliano <julianoh1909@gmail.com>
2023-08-08 13:40:06 -0300

    add config to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
+const dev = process.argv.includes('dev');
+
+export default {
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/hyperchess'
+		}
+	},
 	plugins: [sveltekit()],
 	ssr: {
 		noExternal: ['three']
-	},
-	build: {
-		// generate manifest.json in outDir
-		manifest: true,
-		rollupOptions: {
-			// overwrite default .html entry
-			input: '/path/to/main.js'
-		}
 	}
-});
+};
