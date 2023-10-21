d4a0f9a0d769d0480ac7ff5c03316e919e806f61
juliano <julianoh1909@gmail.com>
2023-10-21 15:46:39 -0300

    set a placeholder logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,14 @@
 <script>
+	import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
-<header class="glassmorphism flex justify-end">
-	<button type="button" class="btn variant-soft-primary">Login</button>
+<header class="glassmorphism">
+	<div />
+
+	<div class="flex justify-end">
+		<button type="button" class="z-50 btn variant-soft-primary">Login</button>
+	</div>
+	<div class="z-10 absolute left-0 top-0 w-full h-full flex justify-center">
+		<Avatar src="/assets/logo_large.png" width="w-20" rounded="rounded-full" />
+	</div>
 </header>
